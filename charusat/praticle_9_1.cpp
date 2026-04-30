df38814715f839970de0b4a31501884cbc1b930d
Mihir1458 <mihirgondaliya263@gmail.com>
2026-04-30 09:44:05 +0530

    cpp project

--- charusat/praticle_9_1.cpp
+++ charusat/praticle_9_1.cpp
@@ -9,22 +9,38 @@ int main()
     cout << "Enter size: ";
     cin >> n;
 
-    vector<int> v(n);
-
+    /*vector<int> v(n); // this can be also used but in this case vector is fixed size and we cannot add more elements to it.
+ Enter size: 5
+ Enter elements:
+ 1
+ 2
+ 3
+ 4
+ 5
+ Array : 0 0 0 0 0 1 2 3 4 5 // we had ireated distaly for 2n times to display all element
+ Reversed using reverse function :
+ 5 4 3 2 1
+ Reversed  manually:
+ 1 2 3 4 5
+  */
+    vector<int> v;
     cout << "Enter elements:" << endl;
     for (int i = 0; i < n; i++)
     {
-        cin >> v[i];
+        // cin >> v[i]; // this is used when we gave size to vector
+        int X;
+        cin >> X;
+        v.push_back(X);
     }
 
     cout << "Array : ";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n + n; i++)
     {
         cout << v[i] << " ";
     }
     cout << endl;
 
-    reverse(v.begin(), v.end());
+   reverse(v.begin(), v.end());
 
     cout << "Reversed using reverse function :  " << endl;
     for (int i = 0; i < n; i++)
